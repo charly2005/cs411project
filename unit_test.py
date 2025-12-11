9ef8e99d03923f55bb6a790300f2af3c9902daf4
Nofer Xue <nortusx@gmail.com>
2025-12-10 21:56:18 -0500

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -24,7 +24,7 @@ class TestMyModule(unittest.TestCase):
                 vitals=vitals
             )
         )
-        print("Gemini Raw Output: ", gemini_raw)
+        # print("Gemini Raw Output: ", gemini_raw)
         assert gemini_raw is not None
 
     def test_recommendation(self):
@@ -38,11 +38,12 @@ class TestMyModule(unittest.TestCase):
         )
         
         rec = recommend_facilities(decision, 34.032845, -118.266266)
-        print("Recommendation Output: ", rec)
+        # print("Recommendation Output: ", rec)
         assert rec is not None
 
-    def test_red_flag_1(self):
+    def test_red_flag(self):
         print("==Testing red flag rules...")
+        print("====Testing red flag [chest pain]...")
 
         text = "Mild chest pain and shortness of breath"
         vitals = Vitals(
@@ -55,39 +56,28 @@ class TestMyModule(unittest.TestCase):
                 text=text,
                 vitals=vitals
             )
-        
-        gemini_raw = call_gemini_for_triage(
-            symptom
-        )
+        gemini_raw = call_gemini_for_triage(symptom)
 
-        decision = apply_rule_safety_layer(symptom, gemini_raw)
+        apply_rule_safety_layer(symptom, gemini_raw)
 
-        print("Gemini Raw Output: ", gemini_raw.get("red_flags", []) or [])
+        # print("Gemini Raw Output: ", gemini_raw.get("red_flags", []) or [])
         assert int(gemini_raw.get("score", 2)) > 3
 
-    def test_red_flag_2(self):
+        print("====Testing red flag [Can't breathe]...")
+        
         text = "Can't breathe properly"
-        vitals = Vitals(
-            temperature_c=36.5,
-            pain_score=0, 
-            pregnant=False, 
-            trauma=False
-        )
         symptom = SymptomInput(
                 text=text,
                 vitals=vitals
             )
-        
-        gemini_raw = call_gemini_for_triage(
-            symptom
-        )
+        gemini_raw = call_gemini_for_triage(symptom)
 
-        decision = apply_rule_safety_layer(symptom, gemini_raw)
+        apply_rule_safety_layer(symptom, gemini_raw)
 
-        print("Gemini Raw Output: ", gemini_raw.get("red_flags", []) or [])
         assert int(gemini_raw.get("score", 2)) > 2
 
-    def test_red_flag_3(self):
+        print("====Testing red flag [High temperature]...")
+        
         text = "Feeling ok"
         vitals = Vitals(
             temperature_c=41.1,
@@ -99,24 +89,43 @@ class TestMyModule(unittest.TestCase):
                 text=text,
                 vitals=vitals
             )
+        gemini_raw = call_gemini_for_triage(symptom)
+
+        apply_rule_safety_layer(symptom, gemini_raw)
         
-        gemini_raw = call_gemini_for_triage(
-            symptom
-        )
-
-        decision = apply_rule_safety_layer(symptom, gemini_raw)
-
-        print("Gemini Raw Output: ", gemini_raw.get("red_flags", []) or [])
         assert int(gemini_raw.get("score", 2)) > 2
 
     def test_geo_code(self):
         print("==Testing geocoding...")
-
-        address = "02134"
+        print("====Testing geocoding [Normal]...")
         
         try:
-            lat, lon, formatted = geocode_address(address)
-            print(f"Geocoded Address: {formatted}, Lat: {lat}, Lon: {lon}")
+            lat, lon, formatted = geocode_address("02134")
+            # print(f"Geocoded Address: {formatted}, Lat: {lat}, Lon: {lon}")
+            assert lat > 0 and lon < 0
+        except GeocodingError as e:
+            self.fail(f"GeocodingError raised: {e}")
+            
+        print("====Testing geocoding [Zero code]...")
+        
+        try:
+            lat, lon, formatted = geocode_address("00000")
+            self.fail(f"Expected GeocodingError, got none")
+        except GeocodingError as e:
+            pass
+            
+        print("====Testing geocoding [Wrong format]...")
+        
+        try:
+            lat, lon, formatted = geocode_address("2134")
+            self.fail(f"Expected GeocodingError, got none")
+        except GeocodingError as e:
+            pass
+            
+        print("====Testing geocoding [Alternative format]...")
+        
+        try:
+            lat, lon, formatted = geocode_address("02134-0000")
             assert lat > 0 and lon < 0
         except GeocodingError as e:
             self.fail(f"GeocodingError raised: {e}")
@@ -138,8 +147,8 @@ class TestMyModule(unittest.TestCase):
         )
 
         records = load_history()
-        print("Loaded History Records: ", records)
-        assert len(records) > 0
+        # print("Loaded History Records: ", records)
+        assert len(records) > 0 and records[-1].urgency_level == "CLINIC"
 
 if __name__ == "__main__":
     unittest.main()
